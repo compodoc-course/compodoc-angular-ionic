32da1c900fb5f190096510abcf7a857e9a6f0d08
Anartz Mugika <mugan86@gmail.com>
2018-07-14 20:43:31 +0200

    Adapt template correctly to shared components and available togled sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { NotpagefoundComponent } from './notpagefound/notpagefound.component';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
 import { FooterComponent } from './footer/footer.component';
+import { ScrollTopComponent } from './scroll-top/scroll-top.component';
 
-
-const COMPONENTS = [HeaderComponent, SidebarComponent, BreadcrumbComponent, NotpagefoundComponent, FooterComponent];
+const COMPONENTS = [HeaderComponent, SidebarComponent, BreadcrumbComponent, NotpagefoundComponent, FooterComponent, ScrollTopComponent];
 
 @NgModule({
-    imports: [],
+    imports: [RouterModule],
     declarations: COMPONENTS,
     exports: COMPONENTS
 })
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  findClassToggled = 'sidenav-toggled';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  // sidenav-toggled
+
+  hello() {
+    console.log('hello');
   }
 
+  toggled(toggler: boolean = false) {
+    const find = document.getElementsByClassName(this.findClassToggled)[0];
+    if (find !== undefined) {
+      // remove
+      find.classList.remove(this.findClassToggled);
+    } else {
+      const takePageTopId = document.getElementById('page-top');
+      // add
+      takePageTopId.classList.add(this.findClassToggled);
+    }
+    console.log('click');
+  }
 }
