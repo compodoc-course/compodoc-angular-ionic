ea6fde002c96d0492c84b4988ad9377279033b9d
Anartz Mugika <mugan86@gmail.com>
2018-07-15 18:57:10 +0200

    Add doghnut chart example in charts pages. Pending to add othe types

--- src/app/pages/charts/charts.component.ts
+++ src/app/pages/charts/charts.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-charts',
   templateUrl: './charts.component.html',
   styleUrls: ['./charts.component.css']
 })
-export class ChartsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ChartsComponent {
+  /**
+   * Values to use with dona graphic component
+   */
+  graphics: any = [
+    {
+      labels: ['Christopher Froome', 'Mikel Landa', 'Vincenzo Niballi', 'Nairo Quintana'],
+      data: [24, 30, 44, 2],
+      type: 'doughnut',
+      legend: 'Who has won the tour of france 2017?'
+    },
+    {
+      labels: ['France', 'Croatia'],
+      data: [65, 35],
+      type: 'doughnut',
+      legend: 'Who will win the 2018 soccer world cup in Russia?'
+    },
+    {
+      labels: ['Yes', 'No'],
+      data: [95, 5],
+      type: 'doughnut',
+      legend: 'Do you like Football?'
+    },
+    {
+      labels: ['No', 'Yes'],
+      data: [85, 15],
+      type: 'doughnut',
+      legend: 'Do you like VAR?'
+    }
+  ];
 }
