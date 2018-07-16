3f4941dbbafdf7db976dc44375b016fbb95b0bec
= <mugan86@gmail.com>
2018-07-16 07:19:04 +0200

    Add new charts and update table

--- src/app/pages/charts/chart.component.ts
+++ src/app/pages/charts/chart.component.ts
@@ -20,7 +20,7 @@ export class ChartComponent {
       labels: ['France', 'Croatia'],
       data: [65, 35],
       type: 'doughnut',
-      legend: 'Who will win the 2018 soccer world cup in Russia?'
+      legend: 'Who has won the 2018 soccer world cup?'
     },
     {
       labels: ['Yes', 'No'],
@@ -35,4 +35,58 @@ export class ChartComponent {
       legend: 'Do you like VAR?'
     }
   ];
+
+  // lineChart
+  public lineChartData: Array<any> = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: '2016'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: '2017'},
+    {data: [18, 48, 77, 9, 100, 27, 40], label: '2018'}
+  ];
+  public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartOptions: any = {
+    responsive: true
+  };
+  public lineChartColors: Array<any> = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.2)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
+  public lineChartLegend = true;
+  public lineChartType = 'line';
+
+  // Base chart
+  public barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels: string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+
+  public barChartData: any[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+
 }
