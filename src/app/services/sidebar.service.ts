3b64dfc8a5557acc209cc627c27a671d58a14a1e
= <mugan86@gmail.com>
2018-07-17 07:06:36 +0200

    Start to add menu side with servce

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -6,4 +6,43 @@ import { Injectable } from '@angular/core';
 export class SidebarService {
 
   constructor() { }
+  menu: any = [
+    {
+      title: 'Dashboard',
+      icon: 'fas fa-tachometer-alt',
+      route: '/dashboard'
+    },
+    {
+      title: 'Charts',
+      icon: 'fas fa-chart-line',
+      route: '/charts'
+    },
+    {
+      title: 'Tables',
+      icon: 'fas fa-table',
+      route: '/tables'
+    },
+    {
+      title: 'Components',
+      icon: 'fab fa-elementor',
+      route: '',
+      submenu: [
+        { title: 'Navbar', url: '/navbar' },
+        { title: 'Cards', url: '/cards' },
+        { title: 'Maps', url: '/maps' },
+        { title: 'Video Player', url: '/video' },
+      ]
+    },
+    {
+      title: 'Example pages',
+      icon: 'fas fa-th-list',
+      route: '',
+      submenu: [
+        { title: 'Navbar', url: '/navbar' },
+        { title: 'Cards', url: '/cards' },
+        { title: 'Maps', url: '/maps' },
+        { title: 'Video Player', url: '/video' },
+      ]
+    }
+  ];
 }
