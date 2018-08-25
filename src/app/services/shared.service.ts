b7a07c188db5c31f77f5982fd85d0046abe2814b
Anartz Mugika <mugan86@gmail.com>
2018-08-25 20:15:04 +0200

    Manage select page to change breadcrum title

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Output, EventEmitter } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
+  private selectPage: string;
+  constructor() {}
 
-  constructor() { }
+  @Output()
+  change: EventEmitter<string> = new EventEmitter();
+
+  setSelectPage(page: string) {
+    this.change.emit(page);
+  }
 }
