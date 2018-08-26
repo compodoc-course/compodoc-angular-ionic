43f2bad92e50e760426a494f1e291eea671dcebd
Anartz Mugika <mugan86@gmail.com>
2018-08-26 10:12:11 +0200

    Add support page

---
+++ src/app/pages/support/support.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../../services/shared.service';
+
+@Component({
+  selector: 'app-support',
+  templateUrl: './support.component.html',
+  styleUrls: ['./support.component.css']
+})
+export class SupportComponent implements OnInit {
+  constructor(private selectPageService: SharedService) {}
+
+  ngOnInit() {
+    this.selectPageService.setSelectPage('Support');
+  }
+}
