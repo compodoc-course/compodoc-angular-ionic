a62fcdce737085c5aa022dcc2200c863ae63004d
Anartz Mugika <mugan86@gmail.com>
2018-09-05 08:04:27 +0200

    Define client CSS, component and service

--- src/app/pages/client/client.component.ts
+++ src/app/pages/client/client.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../../services/shared.service';
+import { ClientService } from './client.service';
+import { RoundPipe } from '../../pipes/round.pipe';
+import { AlertService } from '../../services/alert.service';
 
 @Component({
   selector: 'app-client',
@@ -7,11 +10,43 @@ import { SharedService } from '../../services/shared.service';
   styleUrls: ['./client.component.css']
 })
 export class ClientComponent implements OnInit {
-
-  constructor(private selectPageService: SharedService) { }
+  currentPage = 1;
+  selectClient: Client;
+  clientsList: Array<Client>;
+  totalResults: number;
+  showInfo = false;
+  pages: number[] = [];
+  constructor(
+    private selectPageService: SharedService,
+    private clientService: ClientService,
+    private alertService: AlertService
+  ) {}
 
   ngOnInit() {
     this.selectPageService.setSelectPage('Client');
+    this.loadData();
   }
 
+  loadData() {
+    this.alertService.loadData('Cargando datos de los clientes', '¡¡En un instante accederás!!');
+    this.clientService.getAll(this.currentPage).subscribe(data => {
+      this.clientsList = data.items;
+      console.log(this.clientsList);
+      this.totalResults = data.total_count;
+      let pages = this.totalResults / 10;
+      if (this.totalResults % 10 > 0) {
+        pages++;
+      }
+
+      this.pages = Array(new RoundPipe().transform(pages, 0)).fill(0);
+      this.showInfo = true;
+      this.alertService.closeAlert();
+    });
+  }
+
+  selectPage(select) {
+    console.log(select);
+    this.currentPage = select;
+    this.loadData();
+  }
 }
--- src/app/pages/client/client.service.ts
+++ src/app/pages/client/client.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/internal/operators/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+  API_URL = 'https://gestihotels.mugan86.com/';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+  constructor(private _http: HttpClient) {
+    console.log('Client Service running');
+  }
+
+  private getQuery(query: String) {
+    return this._http.get(`${this.API_URL}${query}`);
+  }
+
+  getAll(page: number = 1) {
+    const data = { token: 'd1a9356cbed83ecd0215afa478610c38', page: page };
+    return this._http
+      .post<any>(this.API_URL + 'api/clients', data, this.httpOptions)
+      .pipe(
+        map(res => {
+          console.log(res.items);
+          return res;
+        })
+      );
+  }
+
+  getSelectItem(id: number) {
+    const data = { token: 'd1a9356cbed83ecd0215afa478610c38', id: id };
+    return this._http
+      .post<Client>(this.API_URL + 'api/client', data, this.httpOptions)
+      .pipe(
+        map(((res: Client) => {
+          console.log(res);
+          return res;
+        })
+      ));
+  }
+
+  setClient(client: Client) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this._http.post<Client>(this.API_URL, client, httpOptions).pipe(
+      map(res => {
+        console.log(res.name);
+        return res;
+      }));
+  }
 }
+
