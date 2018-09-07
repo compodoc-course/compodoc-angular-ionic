d13ce9638384d2180104a8f7de17332924510a2d
Anartz Mugika <mugan86@gmail.com>
2018-09-07 18:24:20 +0200

    Adapt login without input credentials

--- src/app/login/login-form/login.component.ts
+++ src/app/login/login-form/login.component.ts
@@ -10,15 +10,7 @@ import { LocalStorageService } from '../../services/local-storage.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  login: Login = {
-    _username: '',
-    _password: '',
-    _target_path: '',
-    _failure_path: '/api/user/fail'
-  };
-
   constructor(
-    private _login: LoginService,
     private router: Router,
     private alertService: AlertService,
     private localStorage: LocalStorageService
@@ -27,23 +19,13 @@ export class LoginComponent implements OnInit {
   ngOnInit() {}
 
   makeLogin() {
-    this.login._target_path = `/api/users/${this.login._username}`;
-    this.alertService.loadData(
-      'Comprobando los datos de acceso',
-      '¡¡En un instante accederás!!'
-    );
-    this._login.setLogin(this.login).subscribe(
-      data => {
-        // this.router.navigate(['/heroe', data.name]);
-        console.log(data);
-        this.alertService
-  .closeAlert();
-        this.localStorage.saveUserCredentials(data);
-        this.router.navigate(['/dashboard']);
-      },
-      error => console.error(error)
-    );
-    // {email: '...', password: '...'}
-    // ... <-- now use JSON.stringify() to convert form values to json.
+    this.localStorage.saveDataInLocal('user_credentials',
+    {
+      email: 'anartz@compodoc.com',
+      name: 'Anartz',
+      lastname: 'Mugika',
+      role: 'ROLE_ADMIN'
+    }, true);
+    this.router.navigate(['/dashboard']);
   }
 }
