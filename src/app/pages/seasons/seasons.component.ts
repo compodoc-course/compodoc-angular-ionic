c09f60aafa488c981df3d00671cbfc41675a9918
Anartz Mugika <mugan86@gmail.com>
2018-09-08 19:38:48 +0200

    Add season

---
+++ src/app/pages/seasons/seasons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seasons',
+  templateUrl: './seasons.component.html',
+  styleUrls: ['./seasons.component.css']
+})
+export class SeasonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
