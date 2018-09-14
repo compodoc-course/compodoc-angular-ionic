a7bab37bf1c5635ce7c39d0c828434e5bb360c0c
Anartz Mugika <mugan86@gmail.com>
2018-09-14 09:22:10 +0200

    Load races data from server and local file

--- src/app/services/api/races.service.ts
+++ src/app/services/api/races.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment.prod';
+import { RACES_URLS } from '../../constants/urls';
+import { RequestService } from './request.service';
+import { map } from 'rxjs/internal/operators/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RacesService {
-  private url = environment.url;
-  constructor() {}
+  constructor(private request: RequestService) {}
 
   // Race list of select season
   // https://ergast.com/api/f1/<year>.json
@@ -16,4 +17,17 @@ export class RacesService {
 
   // ranking result of select race
   // https://ergast.com/api/f1/<year>/<number-race>/results.json
+
+  listByYear(year) {
+    console.log(year + RACES_URLS.ALL_RACES_SELECT);
+    return this.request
+      .getQuery(year + RACES_URLS.ALL_RACES_SELECT)
+      .pipe(map(data => data['MRData'].RaceTable.Races));
+  }
+
+  loadListFromLocal() {
+    return this.request
+      .getJSON('./../../../assets/data/api/races2018.json')
+      .pipe(map(data => data['MRData'].RaceTable.Races));
+  }
 }
