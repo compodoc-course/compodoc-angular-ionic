db5d61fe928566c698ab1ca478282e7177f3fdcd
Anartz Mugika <mugan86@gmail.com>
2018-09-14 09:21:30 +0200

    Take data to seasons from server and from local file

--- src/app/services/api/seasons.service.ts
+++ src/app/services/api/seasons.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { SEASONS_URLS } from '../../constants/urls';
+import { RequestService } from './request.service';
+import { map } from 'rxjs/internal/operators/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeasonsService {
+  constructor(private request: RequestService) {}
 
-  constructor() { }
+  list() {
+    console.log(SEASONS_URLS.ALL_SEASONS_URL);
+    return this.request
+      .getQuery(SEASONS_URLS.ALL_SEASONS_URL)
+      .pipe(map(data => data['MRData'].SeasonTable.Seasons));
+  }
+
+  loadListFromLocal() {
+    return this.request
+      .getJSON('./../../../assets/data/api/seasons.json')
+      .pipe(map(data => data['MRData'].SeasonTable.Seasons));
+  }
 }
