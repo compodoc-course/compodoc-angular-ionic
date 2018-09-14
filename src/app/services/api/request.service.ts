70557ae5891359e345833e272d4d793296049dff
Anartz Mugika <mugan86@gmail.com>
2018-09-14 09:21:53 +0200

    Add JSON load function

--- src/app/services/api/request.service.ts
+++ src/app/services/api/request.service.ts
@@ -2,20 +2,20 @@ import { Injectable } from '@angular/core';
 import { URL_LOCALHOST } from '../../constants/urls';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/internal/operators/map';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
-
   API_URL = URL_LOCALHOST;
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
     })
   };
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {}
 
   getQuery(query: String) {
     return this._http.get(`${this.API_URL}${query}`);
@@ -25,10 +25,14 @@ export class RequestService {
     return this._http
       .post(this.API_URL + resourceUrl, data, this.httpOptions)
       .pipe(
-        map(((res: any) => {
+        map((res: any) => {
           console.log(res);
           return res;
         })
-        ));
+      );
+  }
+
+  public getJSON(path: string): Observable<any> {
+    return this._http.get(path);
   }
 }
