fe8a09cb9f1fa9e61ac1df6a1ccec7f877f5b107
Anartz Mugika <mugan86@gmail.com>
2018-09-15 07:29:01 +0200

    Add instructions need to activate API services to use in Angular App

--- src/environments/api-keys.ts
+++ src/environments/api-keys.ts
@@ -1,1 +1,13 @@
-export const GOOGLEMAPS = '<ADD_YOUR_API_KEY_HERE';
+/**
+ * You need activate APIS:
+ * Google Static Map API.
+ * Google Maps JavaScript API.
+ * To enable, go to -> Google API Console.
+ * Select Project.
+ * APIs and services -> Library (Biblioteca)
+ * Select Google Maps APIs:
+ * https://console.developers.google.com/apis/library?filter=category:maps&project=[your_project]&folder&organizationId
+ * Select and enable need activate APIs
+ */
+export const GOOGLEMAPS = 'ADD_YOUR_BROWSER_API_KEY_WITH_FOLLOW_INSTRUCTIONS';
+
