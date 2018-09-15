ada74af3977d7258276ca686c7933de2cd6e17f9
Anartz Mugika <mugan86@gmail.com>
2018-09-15 09:50:26 +0200

    Race page implement and use with pipes

--- src/app/pages/races/races.component.ts
+++ src/app/pages/races/races.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../../services/shared.service';
+import { AlertService } from '../../services/alert.service';
+import { RacesService } from '../../services/api/races.service';
+import { Race } from '../../interfaces/race.interface';
 
 @Component({
-  selector: 'app-races',
-  templateUrl: './races.component.html',
-  styleUrls: ['./races.component.css']
+  selector: `app-races`,
+  templateUrl: `./races.component.html`,
+  styleUrls: [`./races.component.css`]
 })
 export class RacesComponent implements OnInit {
-  constructor(private selectPageService: SharedService) {}
+  showInfo = false;
+  racesList: Race[];
+  selectYear: string;
+  constructor(
+    private selectPageService: SharedService,
+    private _racesService: RacesService,
+    private _alertService: AlertService
+  ) {}
 
   ngOnInit() {
-    this.selectPageService.setSelectPage('races');
+    this.selectPageService.setSelectPage(`races`);
+    this.loadDataFromAssets();
+  }
+
+  private loadDataDialog(open: boolean = true) {
+    if (open) {
+      this._alertService.loadData(
+        `Load ${this.selectYear} races list!!`,
+        `Wait a moment please!!`
+      );
+    } else {
+      this._alertService.closeAlert();
+    }
+  }
+
+  /**
+   * Take data from a local file in json format
+   */
+  loadDataFromAssets() {
+    this.selectYear = `2018`;
+    this.loadDataDialog();
+    this._racesService.loadListFromLocal().subscribe((data: Race[]) => {
+      this.selectPageService.setSelectPage(`${this.selectYear} year races`);
+      console.log(data);
+      this.showInfo = true;
+      this.racesList = data;
+      this.loadDataDialog(false);
+    });
   }
 }
