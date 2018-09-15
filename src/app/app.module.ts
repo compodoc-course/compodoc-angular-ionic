df7ae03e4ddaf8ca2e245041d51c0098ec4df3da
Anartz Mugika <mugan86@gmail.com>
2018-09-15 09:48:52 +0200

    Refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { LoginModule } from './login/login.module';
 
 import { AppComponent } from './app.component';
 import { APP_ROUTES } from './app.routes';
+
 @NgModule({
   declarations: [
     AppComponent
