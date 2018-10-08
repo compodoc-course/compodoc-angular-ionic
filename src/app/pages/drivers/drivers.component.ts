dde43c93bd1879ab542a5b9bc82d2a1612d31198
Anartz Mugika <mugan86@gmail.com>
2018-10-08 19:36:59 +0200

    Add jdoctags to show example in drivers.component.ts

--- src/app/pages/drivers/drivers.component.ts
+++ src/app/pages/drivers/drivers.component.ts
@@ -6,31 +6,50 @@ import { AlertService } from '../../services/alert.service';
 import swal from 'sweetalert2';
 
 @Component({
-  selector: 'app-drivers',
-  templateUrl: './drivers.component.html',
-  styleUrls: ['./drivers.component.css']
+  selector: `app-drivers`,
+  templateUrl: `./drivers.component.html`,
+  styleUrls: [`./drivers.component.css`]
 })
 export class DriversComponent implements OnInit {
+  /**
+   * @ignore
+   */
   selectYear: string;
+  /**
+   * @ignore
+   */
   driversList: Driver[];
+  /**
+   * @ignore
+   */
   showInfo: boolean;
   constructor(
     private selectPageService: SharedService,
     private _driversService: DriversService,
     private _alertService: AlertService
   ) {}
-
+  /**
+   * @ignore
+   */
   ngOnInit() {
     this.showInfo = false;
-    this.selectPageService.setSelectPage('drivers');
+    this.selectPageService.setSelectPage(`drivers`);
     this.loadDataFromAssets();
   }
 
+  /**
+   * Function to show / hide loading data loading.
+   * @example
+   * This is a good example to show loading data dialog
+   * loadDataDialog(true)
+   *
+   * @param open Value to specify if open load data dialog
+   */
   private loadDataDialog(open: boolean = true) {
     if (open) {
       this._alertService.loadData(
         `Load ${this.selectYear} season drivers!!`,
-        'Wait a moment please!!'
+        `Wait a moment please!!`
       );
     } else {
       this._alertService.closeAlert();
@@ -38,7 +57,14 @@ export class DriversComponent implements OnInit {
   }
 
   /**
-   * Take data from server
+   * Take data from server. More info in:
+   *  [Drivers List API Documentation]{@link https://ergast.com/mrd/methods/drivers/}
+   *
+   * @example
+   * This is a good example to show 2018 drivers list
+   * selectYearDrivers(2018)
+   *
+   * @param {string} year  Select year value
    */
   selectYearDrivers(year: string = String(new Date().getFullYear())) {
     this.selectYear = year;
@@ -62,7 +88,7 @@ export class DriversComponent implements OnInit {
    * Take data from a local file in json format
    */
   loadDataFromAssets() {
-    this.selectYear = '2018';
+    this.selectYear = `2018`;
     this.loadDataDialog();
     this._driversService.loadListFromLocal().subscribe((data: Driver[]) => {
       console.log(data);
@@ -72,18 +98,21 @@ export class DriversComponent implements OnInit {
     });
   }
 
+  /**
+   * @param {Driver} driver  Select driver object
+   * @returns
+   */
   infoAlert(driver: Driver) {
-
     swal({
       title: `<br/><strong> ${driver.givenName} ${driver.familyName} </strong>`,
       html: ``,
       showCloseButton: false,
       showCancelButton: false,
       focusConfirm: false,
-      confirmButtonText: 'Close',
-      confirmButtonAriaLabel: 'Thumbs up, great!',
-      cancelButtonText: '<i class="fa fa-thumbs-down"></i>',
-      cancelButtonAriaLabel: 'Thumbs down'
+      confirmButtonText: `Close`,
+      confirmButtonAriaLabel: `Thumbs up, great!`,
+      cancelButtonText: `<i class="fa fa-thumbs-down"></i>`,
+      cancelButtonAriaLabel: `Thumbs down`
     });
   }
 }
